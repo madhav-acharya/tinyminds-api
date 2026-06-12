c478e30346d47f1cf1744dbc8e9b1ee7fa645faf
Aastha Khanal <aasthakhanal02@gmail.com>
2026-06-12 23:49:22 +0545

    implemented institution crud and interfaces

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { InstitutionModule } from './institution/institution.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, InstitutionModule],
   controllers: [AppController],
   providers: [AppService],
 })
