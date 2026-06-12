f60bc7d59a48eb7ed2dedf9a74f367a72efb1ed2
Rohan Poudel <yitsmerohan@gmail.com>
2026-06-13 03:27:06 +0545

    added new thing

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*' });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
