40437b412068cc3f9533f54c2ef0a2c4726b6853
Rohan Poudel <yitsmerohan@gmail.com>
2026-06-13 01:59:41 +0545

    feat(app): register GradeModule in AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,14 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
 import { InstitutionModule } from './institution/institution.module';
+import { GradeModule } from './grade/grade.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     CommonModule,
     InstitutionModule,
+    GradeModule,
     UserModule,
     AuthModule,
   ],
