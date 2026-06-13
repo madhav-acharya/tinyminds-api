454d4a01dd032dc0316b47279faa7f005c751123
Madhav Acharya <np05cp4s240068@iic.edu.np>
2026-06-13 10:01:56 +0545

    included the users profile in the user response

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -80,9 +80,18 @@ export class UserService {
       ];
     }
 
+    const include: Prisma.UserInclude = {
+      adminProfile: true,
+      teacherProfile: true,
+      parentProfile: true,
+      learnerProfile: true,
+      ownerProfile: true,
+    };
+
     const [data, total] = await Promise.all([
       this.prisma.user.findMany({
         where,
+        include,
         skip,
         take: limit,
         orderBy: { createdAt: 'desc' },
@@ -112,6 +121,13 @@ export class UserService {
   async findOne(id: string) {
     const user = await this.prisma.user.findUnique({
       where: { id },
+      include: {
+        adminProfile: true,
+        teacherProfile: true,
+        parentProfile: true,
+        learnerProfile: true,
+        ownerProfile: true,
+      },
     });
 
     if (!user) {
